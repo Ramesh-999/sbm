18f3eeaebed8f9e2d8f251fffee5ee4614067523
rameshflex <ramesh@flex-work.in>
2025-02-23 15:34:02 +0530

    feat: develop changes

---
+++ smbdigitalway/smbdigitalway/doctype/smb_differentiator_list/smb_differentiator_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Ramesh and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SMB Differentiator List", {
+// 	refresh(frm) {
+
+// 	},
+// });
